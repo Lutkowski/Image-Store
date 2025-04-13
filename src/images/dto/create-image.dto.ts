cfebd2d530cbbbf4d29a683c99bd5e85bdd6a620
Lutkowski <anton.lutkovskiy@gmail.com>
2025-04-14 00:52:42 +0300

    feat(swagger): add swagger and annotations

--- src/images/dto/create-image.dto.ts
+++ src/images/dto/create-image.dto.ts
@@ -1,6 +1,10 @@
 import { IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateImageDto {
+  @ApiPropertyOptional(
+    { description: 'Описание изображения' },
+  )
   @IsOptional()
   @IsString()
   readonly description?: string;
--- src/images/image.entity.ts
+++ src/images/image.entity.ts
@@ -1,19 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Image {
+  @ApiProperty({ example: 1, description: 'Уникальный идентификатор изображения' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'file123456.jpg', description: 'Имя файла' })
   @Column()
   filename: string;
 
+  @ApiProperty({ example: '/uploads/file123456.jpg', description: 'Путь к файлу' })
   @Column()
   path: string;
 
+  @ApiProperty({ example: 'http://localhost:3000/uploads/file123456.jpg', description: 'URL для доступа к файлу' })
   @Column()
   url: string;
 
+  @ApiProperty({ example: 'Фото объекта', description: 'Описание изображения', required: false })
   @Column({ nullable: true })
   description?: string;
 }
