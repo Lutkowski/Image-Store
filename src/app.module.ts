182bdff25b676b0eb292eff58d894537b94bebef
Lutkowski <anton.lutkovskiy@gmail.com>
2025-04-13 17:04:47 +0300

    chore: database config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ImagesModule } from './images/images.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: parseInt(configService.get<string>('POSTGRES_PORT'), 10),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+
+    ImagesModule,
+  ],
   controllers: [],
   providers: [],
 })
